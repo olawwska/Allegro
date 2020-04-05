a4d9d13cc52f485bbd76864a17a2963039c0d318
olawwska <olwojciech@gmail.com>
2020-04-05 22:56:44 +0200

    added ClassComponent and stateto App

--- Desktop/Prework/Allegro/allegro/src/App.js
+++ Desktop/Prework/Allegro/allegro/src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './App.module.scss';
+const Pokedex = require('pokedex-promise-v2');
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
-)
+
+class App extends React.Component {
+  state = {
+    pokemonNames: [],
+    pokemonInfos: []
+  }
+
+  componentDidMount() {
+    const P = new Pokedex();
+
+    P.getPokemonsList()
+      .then(res => {
+        return res
+      }).then(res => {
+        res.results.map(pokemon => {
+          this.setState = {
+            pokemonNames: pokemon.name,
+          }
+          return pokemon.name
+        }).map(pokemonName => {
+          return P.getPokemonByName(pokemonName)
+            .then(res => {
+              console.log(res);
+              this.setState = {
+                pokemonInfos: res
+              }
+              return res
+            })
+        })
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return <div></div>
+  }
+}
 
 export default App;
