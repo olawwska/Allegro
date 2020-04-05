c034a18da48d3f8f55130e56c1ecfbdb315f7650
olawwska <olwojciech@gmail.com>
2020-04-05 19:11:46 +0200

    added fetch and state

--- Desktop/Prework/Allegro/allegro/src/App.js
+++ Desktop/Prework/Allegro/allegro/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.module.scss';
+const Pokedex = require('pokedex-promise-v2');
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
-)
+
+class App extends React.Component {
+  state = {
+    pokemonNames: []
+  }
+
+  componentDidMount() {
+    const P = new Pokedex();
+
+    P.getPokemonsList()
+      .then(res => {
+        return res.results.map(pokemon => {
+          this.setState({
+            pokemonNames: pokemon.name
+          })
+          return pokemon.name
+        })
+      })
+      .catch(err => {
+        console.log('There was an ERROR', err);
+      });
+  }
+
+  render() {
+    return <div>
+      <h1>Hello World</h1>
+    </div>
+  }
+}
 
 export default App;
