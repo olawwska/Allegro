08c5063b70634a3e0bc9af39dda0f6b772c8f375
olawwska <olwojciech@gmail.com>
2020-04-06 18:45:28 +0200

    working fetch that passes pokemon items info to state in App component

--- Desktop/Prework/Allegro/allegro/src/App.js
+++ Desktop/Prework/Allegro/allegro/src/App.js
@@ -4,8 +4,8 @@ const Pokedex = require('pokedex-promise-v2');
 
 
 class App extends React.Component {
+
   state = {
-    pokemonNames: [],
     pokemonInfos: []
   }
 
@@ -17,24 +17,27 @@ class App extends React.Component {
         return res
       }).then(res => {
         res.results.map(pokemon => {
-          this.setState = {
-            pokemonNames: pokemon.name,
-          }
           return pokemon.name
         }).map(pokemonName => {
           return P.getPokemonByName(pokemonName)
             .then(res => {
-              console.log(res);
-              this.setState = {
-                pokemonInfos: res
-              }
-              return res
+              // console.log(res)
+              const pokemonsInfos = [...this.state.pokemonInfos]
+              this.setState({
+                pokemonInfos: [
+                  ...pokemonsInfos, res]
+
+              });
             })
         })
       }).catch(err => {
         console.log(err)
       })
   }
+
+  //z tego zrobić funkcję i wsadzić ją do rendera usuwając stan 
+  //ewentualnie https://stackoverflow.com/questions/47735600/react-setstate-in-a-map-function
+
 
   render() {
     return <div></div>
