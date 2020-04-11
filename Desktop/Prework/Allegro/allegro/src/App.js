62c16908a847f7b1113db33aa2ebd31a8fc3c045
olawwska <olwojciech@gmail.com>
2020-04-11 13:31:36 +0200

    basic pagination

--- Desktop/Prework/Allegro/allegro/src/App.js
+++ Desktop/Prework/Allegro/allegro/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.module.scss';
 import ListItem from './components/ListItem/ListItem';
 import styles from './App.module.scss';
+// import Pagination from '@material-ui/core/Button';
 
 const Pokedex = require('pokedex-promise-v2');
 
@@ -9,10 +10,14 @@ const Pokedex = require('pokedex-promise-v2');
 class App extends React.Component {
 
   state = {
-    pokemonInfos: []
+    offset: 0,
+    pokemonInfos: [],
+    perPage: 10,
+    currentPage: 0,
+    pageCount: null
   }
 
-  componentDidMount() {
+  pokemonGetter() {
     const P = new Pokedex();
 
     P.getPokemonsList()
@@ -24,7 +29,6 @@ class App extends React.Component {
         }).map(pokemonName => {
           return P.getPokemonByName(pokemonName)
             .then(res => {
-              // console.log(res)
               const pokemonsInfos = [...this.state.pokemonInfos]
               this.setState({
                 pokemonInfos: [
@@ -38,29 +42,44 @@ class App extends React.Component {
       })
   }
 
-  pokemonCreate = (pokemons) => {
-    return pokemons.map((onePokemon) => {
-      return <ListItem
-        name={onePokemon.name}
-        image={onePokemon.sprites.front_default}
-        key={onePokemon.name}
-        alt={onePokemon.name}
-      />
-    })
-  };
+  // pokemonCreate = (pokemons) => {
+  //   return pokemons.map((onePokemon) => {
+  //     return <ListItem
+  //       name={onePokemon.name}
+  //       image={onePokemon.sprites.front_default}
+  //       key={onePokemon.name}
+  //       alt={onePokemon.name}
+  //     />
+  //   })
+  // };
 
+  pokemonCreate = (pokemons) => {
+    const slice = pokemons.slice(this.state.offset, this.state.offset + this.state.perPage)
+    const pagedPokemons = slice.map(pp =>
+      <ListItem>
+        name={pp.name}
+        image={pp.sprites.front_default}
+        key={pp.name}
+        alt={pp.name}
+      </ListItem>)
+
+    this.setState({
+      pageCount: Math.ceil(pokemons.length / this.state.perPage),
+      pagedPokemons
+    })
+    //cant perform react setState on an unmounted component To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
+  }
+
+
+  componentDidMount() {
+    this.pokemonGetter()
+  }
 
   render() {
-
-    // const { pokemonInfos } = this.state
+    const { pokemonInfos } = this.state
 
     return <div className={styles.mainWrapper}>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      {/* {this.pokemonCreate(pokemonInfos)} */}
+      {this.pokemonCreate(pokemonInfos)}
     </div>
   }
 }
